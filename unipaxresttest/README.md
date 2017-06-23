3c7d154f9d1188ffa8897d01bac5165ddfa4da2b
sebwink <winkler@informatik.uni-tuebingen.de>
2017-06-23 20:52:08 +0200

    test, notebook

---
+++ unipaxresttest/README.md
@@ -0,0 +1,4 @@
+Tests for the consistency of the REST API itself (subject to bugs in the Python wrapper ;)
+------------------------------------------------------------------------------------------
+
+python -m unittest <test.py>
--- unipaxresttest/graph_regulatory.py
+++ unipaxresttest/graph_regulatory.py
@@ -1,8 +1,25 @@
 import os
+import itertools
 import unittest
 
-import requests
+import unipax.api
 
-class TestGraphRegulatory(unittest.TestCase):
+class RegulatoryGraphFormatConsistency(unittest.TestCase):
 
-    def 
+    formats = ['gml', 'sif', 'graphml']          # lemon reader not implemented yet; (TODO: formats = unipax.api.UniPaxRestApi.graphs.regulatory.formats)
+
+    def setUp(self):
+        self.graphs = {}
+        api = unipax.api.UniPaxRestApi()
+        for frmt in self.formats:
+            self.graphs[frmt] = api.graph.regulatory(format=frmt)
+
+    def test_equal_number_of_nodes(self):
+        for format1, format2 in itertools.combinations(self.formats, 2):
+            self.assertEqual(len(self.graphs[format1].vs), len(self.graphs[format2].vs),
+                             'unequal number of nodes: {} {}'.format(format1, format2))
+
+    def test_equal_number_of_edges(self):
+        for format1, format2 in itertools.combinations(self.formats, 2):
+            self.assertEqual(len(self.graphs[format1].es), len(self.graphs[format2].es),
+                             'unequal number of edges: {} {}'.format(format1, format2))
